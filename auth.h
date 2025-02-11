56cb4f10348da64abb14e0e830a0298c4c35fa27
MartinzereP <158204516+MartinzereP@users.noreply.github.com>
2025-02-10 20:36:24 -0600

    authentication function

---
+++ auth.h
@@ -0,0 +1,14 @@
+#include <iostream> 
+usidng namespace std;
+
+void authenticateUser()
+{
+    string user, pass;
+
+    cout << " Please enter username ";
+    cin >> user;
+
+    cout << "Please enter Password" << endl;
+    cin >> pass;
+    cout << "Welcome ";
+}
